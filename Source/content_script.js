fe550e6b41a44b7c54f3e60458bbc3d05f69cbd8
Andrew Worcester <andrew@amwmedia.com>
2021-08-13 09:15:38 -0400

    initial code changes

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,50 +1,48 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	var tagName = node.tagName ? node.tagName.toLowerCase() : "";
-	if (tagName == 'input' || tagName == 'textarea') {
-		return;
-	}
-	if (node.classList && node.classList.contains('ace_editor')) {
-		return;
-	}
+function walk(node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+  if (tagName == "input" || tagName == "textarea") {
+    return;
+  }
+  if (node.classList && node.classList.contains("ace_editor")) {
+    return;
+  }
+
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+  var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  v = v.replace(
+    /\bduring covid(-?19)?\b/gi,
+    "during explosive diarrhea season"
+  );
+  v = v.replace(/\bcovid(-?19)?\b/gi, "explosive diarrhea");
+  v = v.replace(/\bcoronavirus( disease)?\b/gi, (m) =>
+    m.charCodeAt(0) === 67 ? "Explosive Diarrhea" : "explosive diarrhea"
+  );
+
+  textNode.nodeValue = v;
 }
-
-
